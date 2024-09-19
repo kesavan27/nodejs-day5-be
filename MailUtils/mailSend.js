8bea3a1773ba8fe00cb1f5e19e7c7bfb7e595e5b
kesavan27 <gvkesavan2003@gmail.com>
2024-09-19 08:24:45 +0530

    update

--- MailUtils/mailSend.js
+++ MailUtils/mailSend.js
@@ -6,14 +6,14 @@ dotenv.config();
 const transport = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "nithishkumarmurugesan2001@gmail.com",
+        user: "gvkesavan27@gmail.com",
         pass: process.env.GMAIL_PASSWORD
     }
 });
 
 
 const mailOptions = {
-    from: "nithishkumarmurugesan2001@gmail.com",
+    from: "gvkesavan27@gmail.com",
     to: [],
     subject: "Password Reset Link",
     text: ""
